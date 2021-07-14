443d6a4e2e4dd780544d2e1903c115f9b8199578
Patrik Barnet <Mrr7782@outlook.com>
2021-07-14 20:10:59 +0200

    Removed menu bar

--- src/index.js
+++ src/index.js
@@ -7,19 +7,14 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
   });
-
-  // and load the website
+  mainWindow.setMenuBarVisibility(false);
   mainWindow.loadURL("https://notion.so/");
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -38,6 +33,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
